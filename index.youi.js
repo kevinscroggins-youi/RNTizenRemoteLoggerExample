9a1289cdde6987675030466eea12631c4ad28082
Kevin Scroggins <kevin.scroggins@youi.tv>
2020-07-06 15:19:33 -0400

    Add initial skeleton project files

---
+++ index.youi.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { AppRegistry, View } from "react-native";
+import { FormFactor } from "@youi/react-native-youi";
+
+export default class YiReactApp extends Component {
+  render() {
+    return (
+      <View />
+    );
+  }
+}
+
+AppRegistry.registerComponent("YiReactApp", () => YiReactApp);
