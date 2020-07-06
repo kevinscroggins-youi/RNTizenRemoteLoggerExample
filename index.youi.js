17722dfe600b2bbfd52dc03eca8a357dfcbf0886
Kevin Scroggins <kevin.scroggins@youi.tv>
2020-07-06 15:32:20 -0400

    Add log testing code

--- index.youi.js
+++ index.youi.js
@@ -1,13 +1,74 @@
 import React, { Component } from "react";
-import { AppRegistry, View } from "react-native";
-import { FormFactor } from "@youi/react-native-youi";
+import { AppRegistry, View, StyleSheet } from "react-native";
+import { Input } from "@youi/react-native-youi";
 
 export default class YiReactApp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.messageNumber = 1;
+    this.inputListeners = [];
+  }
+
+  componentDidMount() {
+    this.addAllInputListeners();
+  }
+
+  componentWillUnmount() {
+    this.removeAllInputListeners();
+  }
+
+  addAllInputListeners = () => {
+    this.addInputListeners(["Select", "Enter"], this.onSelectPressed);
+  };
+
+  addInputListeners = (keys, callback) => {
+    keys.forEach((key) => {
+      this.addInputListener(key, callback);
+    });
+  };
+
+  addInputListener = (key, callback) => {
+    Input.addEventListener(key, callback);
+
+    this.inputListeners.push({
+      key,
+      callback,
+    });
+  };
+
+  removeAllInputListeners = () => {
+    this.inputListeners.forEach(({key, callback}) => {
+      Input.removeEventListener(key, callback);
+    });
+
+    this.inputListeners.length = 0;
+  };
+
+  removeInputListener = (key, callback) => {
+    Input.removeEventListener(key, callback);
+
+    this.inputListeners = this.inputListeners.filter(({listenerKey, listenerCallback}) => {
+      return listenerKey !== key &&
+              listenerCallback !== callback;
+    });
+  };
+
+  onSelectPressed = () => {
+    console.log(`Test message #${this.messageNumber++}, please ignore!`);
+  };
+
   render() {
     return (
-      <View />
+      <View style={styles.mainContainer} />
     );
   }
 }
 
 AppRegistry.registerComponent("YiReactApp", () => YiReactApp);
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    backgroundColor: "blue"
+  }
+});
